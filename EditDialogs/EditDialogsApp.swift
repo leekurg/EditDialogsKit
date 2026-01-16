f0e6fa533005559d52ed59d3e87000de3ab7923a
Илья Аникин <anikin.idm@yandex.ru>
2026-01-17 01:37:09 +0300

    Initial Commit

---
+++ EditDialogs/EditDialogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditDialogsApp.swift
+//  EditDialogs
+//
+//  Created by Илья Аникин on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct EditDialogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
